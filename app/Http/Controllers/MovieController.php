87ed9bfcac5b7106be55921514c4bf119ad7b475
francesca <fradeluca85@gmail.com>
2022-02-06 13:35:52 +0100

    stampa titolo

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class MovieController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $movies = Movie::all();
+        // dd($movies);
+        return view("home", ["movies" => $movies]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,7 @@
-<h1>Home</h1>
+
+<ul>
+    @foreach ($movies as $movie)
+    <li>{{$movie['title']}} - {{$movie['original_title']}}</li>
+        
+    @endforeach
+</ul>
